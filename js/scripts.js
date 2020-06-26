18fc3fc8bd23baefd54bc162a3241f5a6c299115
AlchemistOfWeb <nikitakzntcv@gmail.com>
2020-06-26 11:31:34 +0400

    added validation form

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,11 @@
             $(infoSelector).text($(`${checkboxSelector}:checked`).attr('value'));
         });
     }
+    function isACorrectInput($inputSelector){
+        let regularExp = /^.{3,18}$/i;
+        let str = $inputSelector.val();
+        return regularExp.test(str);
+    }
     const BIND_DELAY = 400;
     let lastWheel = new Date();
     let positionSlide = 0;
@@ -28,18 +33,50 @@
     const sectionCount = $('.section-outer').length;
     const SCROLL_MAX = (sectionCount * 100) - 100;
     const SCROLL_MIN = 0;
+    let isCorrectInputName = false;
+    let isCorrectInputSurame = false;
 
     const inputName = $('#name-js');
+    inputName.bind('input',(e)=>{
+        if(isACorrectInput(inputName)){
+            console.log('name is correct');
+            inputName.removeClass('section-header-wrapper__form-input_border-color_red');
+            inputName.addClass('section-header-wrapper__form-input_border-color_green');
+            isCorrectInputName =true;
+        }else{
+            console.log('name is incorrect');
+            inputName.removeClass('section-header-wrapper__form-input_border-color_green');
+            inputName.addClass('section-header-wrapper__form-input_border-color_red');
+            isCorrectInputName = false;
+        }
+    });
     const inputSurname = $('#surname-js');
+    inputSurname.bind('input',()=>{
+        if(isACorrectInput(inputSurname)){
+            console.log('surname is correct');
+            inputSurname.removeClass('section-header-wrapper__form-input_border-color_red');
+            inputSurname.addClass('section-header-wrapper__form-input_border-color_green');
+            isCorrectInputSurame = true;
+        }else{
+            console.log('surname is incorrect');
+            inputSurname.removeClass('section-header-wrapper__form-input_border-color_green');
+            inputSurname.addClass('section-header-wrapper__form-input_border-color_red');
+            isCorrectInputSurame = false;
+        }
+    });
     const personName = $('.js-person-name');
     const personSurname = $('.js-person-surname');
     $swiper.bind('click',(e)=>{
-        personName.text(inputName.val());
-        personSurname.text(inputSurname.val());
-        if(positionSlide > -SCROLL_MAX){
-            positionSlide -=100;
+        if(isCorrectInputSurame && isCorrectInputName){
+            personName.text(inputName.val());
+            personSurname.text(inputSurname.val());
+            if(positionSlide > -SCROLL_MAX){
+                positionSlide -=100;
+            }
+            changePositionSlide($section, positionSlide);
+        } else {
+            alert('Введите корректное имя и фамилию иначе мы вас убьём. (Имя и фамилия должны быть от 3 до 18 символов)');
         }
-        changePositionSlide($section, positionSlide);
     });
     $commonSwiper.click(()=>{
         if(positionSlide > -SCROLL_MAX){
@@ -47,7 +84,6 @@
         }
         changePositionSlide($section, positionSlide);
     });
-
     $(document).bind('mousewheel DOMMouseScroll', (e)=>{
         let nowWheel = new Date();
         if(nowWheel.getTime() - lastWheel.getTime() > BIND_DELAY){
@@ -66,7 +102,6 @@
         }
     });
     // const btnSwiper = $('#swiper-js');
-    
     checkboxLimitation('input[name=crew-member]', 5);
     checkboxChangeInfo('input[name=starship]', '#starship');
     checkboxChangeInfo('input[name=item-equipment]', '#equipment');

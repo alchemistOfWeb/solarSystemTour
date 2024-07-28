9b15d549e72100b88e477bde446b34a386793aad
Alchemist of the web <nikitakzntcv@gmail.com>
2024-07-28 14:24:59 +0400

    start refactoring of js

--- src/js/main.js
+++ src/js/main.js
@@ -16,10 +16,12 @@ import * as popper from "@popperjs/core";
 import * as bootstrap from "bootstrap";
 import tippy from 'tippy.js';
 import ChoicesCarousel from './module/carousel.js';
-
+import * as verification from './module/verify_inputs.js';
 window.$ = $;
 
 function checkboxLimitation(checkboxSelector, num){
+    // deprecated function
+    // TODO: replace with new code
     const $crewMembers = $(checkboxSelector);
     $crewMembers.on("change", ()=>{
         if($(`${checkboxSelector}:checked`).length >= num){
@@ -28,23 +30,34 @@ function checkboxLimitation(checkboxSelector, num){
         }else{
             $(`${checkboxSelector}:disabled`).removeAttr('disabled');
             $crewMembers.removeClass('little-choise-container__radio-input-js_red');
-
         }
     });
 }
 
-function checkboxChangeInfo(checkboxSelector, infoSelector){
+function checkboxChangeInfo(checkboxSelector, infoSelector) {
+    // deprecated handling of a choice
+    // TODO: update for new version of code
     $(checkboxSelector).on("change", ()=>{
         $(infoSelector).text($(`${checkboxSelector}:checked`).attr('value'));
     });
 }
 
-function isACorrectInput($inputSelector){
-    let regularExp = /^.{3,18}$/i;
-    let regularExp2 = /\s/;
-    let str = $inputSelector.val();
-    return regularExp.test(str) && (!regularExp2.test(str));
+
+
+function changePositionSlide($object, newPos) {
+    $object.animate({marginTop: `${newPos}vh`}, 500,'swing');
 }
+
+function infoBarHide($selector, CONST_POS, pos) {
+    // hide bar on a magic position
+    console.log(pos);
+    if (pos > CONST_POS || pos < -400){
+        $selector.animate({'top':'-120px'}, 700, 'swing');
+    } else{
+        $selector.animate({'top':'0'}, 700, 'swing');
+    }
+}
+
 
 
 // const starshipTPLayout = (speed, fuel, health, firepower) => `
@@ -53,7 +66,6 @@ function isACorrectInput($inputSelector){
 // <span>health: ${health}</span>
 // <span>firepower: ${firepower}</span>
 // `;
-
 
 
 // ENTRYPOINT ----------------------------------------
@@ -81,70 +93,33 @@ $(function() {
     console.log("point 1");
     var crewCarousel = new ChoicesCarousel($("#crewCarouselControls")[0], 5); crewCarousel.activate();
 
-    // TODO: change to funtion
-    const changePositionSlide = ($object, newPos)=>{
-        $object.animate({marginTop: `${newPos}vh`}, 500,'swing');
-    };
-
-    // TODO: change to function
-    const infoBarHide = ($selector, CONST_POS, pos)=>{
-        // hide bar on a magic position
-        console.log(pos);
-        if (pos > CONST_POS || pos < -400){
-            $selector.animate({'top':'-120px'}, 700, 'swing');
-            // console.log('yes');
-        } else{
-            $selector.animate({'top':'0'}, 700, 'swing');
-            // console.log('no');
-        }
-    }
-
     const BIND_DELAY = 400;
     let lastWheel = new Date();
     let positionSlide = 0;
     const $section = $('.section-header');
-    const $swiper = $('#swiper-js');
-    const $commonSwiper = $('.swiper-js');
+
+    const $swiper = $('#swiper-js'); 
+    const $commonSwiper = $('.swiper-js'); // next-btn in the botom of each slide
+
     const sectionCount = $('.section-outer').length;
     const SCROLL_MAX = (sectionCount * 100) - 100;
     const SCROLL_MIN = 0;
     const INFO_BAR_HIDE_POS = -100;
-    let isCorrectInputName = false;
-    let isCorrectInputSurame = false;
+
+    window.isCorrectInputName = false;
+    window.isCorrectInputSurname = false;
     const $infoBar = $('.js-info-bar');
-    
-    const inputName = $('#name-js');
-    inputName.on('input',(e)=>{
-        if(isACorrectInput(inputName)){
-            console.log('name is correct');
-            inputName.removeClass('section-header-wrapper__form-input_border-color_red');
-            inputName.addClass('section-header-wrapper__form-input_border-color_green');
-            isCorrectInputName =true;
-        }else{
-            console.log('name is incorrect');
-            inputName.removeClass('section-header-wrapper__form-input_border-color_green');
-            inputName.addClass('section-header-wrapper__form-input_border-color_red');
-            isCorrectInputName = false;
-        }
-    });
-    const inputSurname = $('#surname-js');
-    inputSurname.on('input',()=>{
-        if(isACorrectInput(inputSurname)){
-            console.log('surname is correct');
-            inputSurname.removeClass('section-header-wrapper__form-input_border-color_red');
-            inputSurname.addClass('section-header-wrapper__form-input_border-color_green');
-            isCorrectInputSurame = true;
-        }else{
-            console.log('surname is incorrect');
-            inputSurname.removeClass('section-header-wrapper__form-input_border-color_green');
-            inputSurname.addClass('section-header-wrapper__form-input_border-color_red');
-            isCorrectInputSurame = false;
-        }
-    });
+
+    verification.verifyInputs();
+
     const personName = $('.js-person-name');
     const personSurname = $('.js-person-surname');
+
+    // NEXT BTN HANDLING
+    const inputName = $('#input-name-js');
+    const inputSurname = $('#input-surname-js');
     $swiper.on("click", (e)=>{
-        if(isCorrectInputSurame && isCorrectInputName){
+        if(window.isCorrectInputSurname && window.isCorrectInputName){
             personName.text(inputName.val());
             personSurname.text(inputSurname.val());
             if(positionSlide > -SCROLL_MAX){
@@ -156,6 +131,8 @@ $(function() {
             alert('Введите корректное имя и фамилию. (Имя и фамилия должны быть от 3 до 18 символов и не содержать пробелов)');
         }
     });
+
+
     $commonSwiper.on("click", ()=>{
         if(positionSlide > -SCROLL_MAX){
             positionSlide -=100;
@@ -163,6 +140,9 @@ $(function() {
             changePositionSlide($section, positionSlide);
         }
     });
+    // NEXT BTN HANDLING END
+
+    // SCROLL HANDLING
     $(document).on('mousewheel DOMMouseScroll', (e)=>{
         let nowWheel = new Date();
         if(nowWheel.getTime() - lastWheel.getTime() > BIND_DELAY){
@@ -183,38 +163,48 @@ $(function() {
             }
         }
     });
-    // const btnSwiper = $('#swiper-js');
-    const $checkboxCrewMember = $('input[name=crew-member]');
-    $checkboxCrewMember.on('click', function(e){
-        const id = $(this).prop('id');
-        const title = $(this).prop('value');
-        console.log($(this).attr('data-url-image'));
+    // SCROLL HANDLING END
+    
+    // CHOICE HANDLING
+    // deprecated handling of a choice
+    // TODO: update for new version of code
+    // const $checkboxCrewMember = $('input[name=crew-member]');
+    // $checkboxCrewMember.on('click', function(e){
+    //     const id = $(this).prop('id');
+    //     const title = $(this).prop('value');
+    //     console.log($(this).attr('data-url-image'));
 
-        if($(this).prop('checked')){
-            const imageUrl = $(this).attr('data-url-image');
-            $('<li></li>',{
-                title: title,
-                click: (e)=>{console.log(title)},
-                class: 'info-bar__crew-list-item',
-                id: id + "in-list"
-            }).css({
-                'backgroundImage': `url('./${imageUrl}')`,
-                'backgroundPosition': "center",
-                'backgroundSize': "contain"
-            }).appendTo($('#js-crew-list'));
-            console.log('ok');
-        } else {
-            $("#" + id + "in-list").remove();
-            console.log('none');
-        }
+    //     if($(this).prop('checked')){
+    //         const imageUrl = $(this).attr('data-url-image');
+    //         $('<li></li>',{
+    //             title: title,
+    //             click: (e)=>{console.log(title)},
+    //             class: 'info-bar__crew-list-item',
+    //             id: id + "in-list"
+    //         }).css({
+    //             'backgroundImage': `url('./${imageUrl}')`,
+    //             'backgroundPosition': "center",
+    //             'backgroundSize': "contain"
+    //         }).appendTo($('#js-crew-list'));
+    //         console.log('ok');
+    //     } else {
+    //         $("#" + id + "in-list").remove();
+    //         console.log('none');
+    //     }
 
-    });
-    checkboxLimitation('input[name=crew-member]', 5);
+    // });
+    // limit persons for choice in crew
+    // checkboxLimitation('input[name=crew-member]', 5);
 
-    checkboxChangeInfo('input[name=starship]', '#starship');
-    checkboxChangeInfo('input[name=item-equipment]', '#equipment');
-    checkboxChangeInfo('input[name=flightPath]', '#flightPath');
+    // deprecated handling of a choice
+    // TODO: update for new version of code
+    // checkboxChangeInfo('input[name=starship]', '#starship');
+    // checkboxChangeInfo('input[name=item-equipment]', '#equipment');
+    // checkboxChangeInfo('input[name=flightPath]', '#flightPath');
 
+    // CHOICE HANDLING END
+
+    // INFOBAR
     const $infoBarWrapper = $('.info-bar__wrapper');
     const $btnInfoBarOpen = $('.info-bar__toggle');
 
@@ -231,5 +221,6 @@ $(function() {
         $btnInfoBarOpen.removeClass('info-bar__toggle-arrow_up');
         $infoBarWrapper.removeClass('info-bar_open');
     });
+    // INFOBAR END
 });
 
---
+++ src/js/module/verify_inputs.js
@@ -0,0 +1,46 @@
+import $ from "jquery";
+
+export function isACorrectInput($inputSelector){
+    let regularExp = /^.{3,18}$/i;
+    let regularExp2 = /\s/;
+    let str = $inputSelector.val();
+    return regularExp.test(str) && (!regularExp2.test(str));
+}
+
+export function verifyInputs(values=null) {
+    console.log("verifyInputs");
+    const inputName = $('#input-name-js');
+    console.log("verifyInputs", inputName);
+    inputName.on('input', (e)=>{
+        console.log("verifyInputs on input name");
+        if(isACorrectInput(inputName)){
+            console.log("is correct input");
+            inputName.removeClass('border-focus-danger');
+            inputName.removeClass('border-focus-normal');
+            inputName.addClass('border-focus-success');
+            window.isCorrectInputName = true;
+        }else{
+            console.log("isnt correct input");
+            inputName.removeClass('border-focus-success');
+            inputName.removeClass('border-focus-normal');
+            inputName.addClass('border-focus-danger');
+            window.isCorrectInputName = false;
+        }
+    });
+    const inputSurname = $('#input-surname-js');
+    inputSurname.on('input',()=>{
+        if(isACorrectInput(inputSurname)){
+            console.log('surname is correct');
+            inputName.removeClass('border-focus-danger');
+            inputName.removeClass('border-focus-normal');
+            inputName.addClass('border-focus-success');
+            window.isCorrectInputSurname = true;
+        }else{
+            console.log('surname is incorrect');
+            inputName.removeClass('border-focus-success');
+            inputName.removeClass('border-focus-normal');
+            inputName.addClass('border-focus-danger');
+            window.isCorrectInputSurname = false;
+        }
+    });
+}
